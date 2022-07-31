685178c7f1e98055f92a883fc136d2d294d336b0
Cleggatron <david.robert.clegg@gmail.com>
2022-07-31 15:37:07 +0100

    added cbasic components

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,7 +1,29 @@
+import {useEffect, useState} from "react";
+
 function Home(){
+
+    const [workouts, setWorkouts] = useState(null);
+
+    useEffect(() => {
+        const fetchWorkouts = async () => {
+            const response = await fetch("http://localhost:3000/api/workouts");
+            const json = await response.json();
+
+            if(response.ok){
+                setWorkouts(json);
+            }
+        }
+
+        fetchWorkouts();
+    }, [])
+
     return(
         <div className="home">
-            <h2>Home</h2>
+            <div className="workouts">
+                {workouts && workouts.map((workout) => {
+                    <p key={workout.id}>{workout.title}</p>
+                })}
+            </div>
         </div>
     )
 }

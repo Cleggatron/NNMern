0e809effb1c57626597d7fa2a40ec514ea534378
Cleggatron <david.robert.clegg@gmail.com>
2022-07-31 09:32:50 +0100

    added basic routes

--- server/routes/workouts.js
+++ server/routes/workouts.js
@@ -1,8 +1,29 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", () => {
+//Placeholder get all workouts
+router.get("/", (req, res) => {
+    res.json({message: "Get all workouts"});
+})
 
+//Placeholder get a single workout
+router.get("/:id", (req, res) => {
+    res.json({message: "Get a single workout"});
+})
+
+//Placeholder Post a workout
+router.post("/", (req, res) => {
+    res.json({message: "Post a new workout"})
+})
+
+//Placeholder delete a workout
+router.delete("/:id", (req, res) => {
+    res.json({message: "Delete a new workout"})
+})
+
+//Placeholder update a workout
+router.patch("/", (req, res) => {
+    res.json({message: "Update a new workout"})
 })
 
 module.exports = router;

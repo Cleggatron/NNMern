fd2bd56451d3028858aeadabf9b7a698b99d21cf
Cleggatron <david.robert.clegg@gmail.com>
2022-07-31 09:19:49 +0100

    initial express setup

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+require("dotenv").config();
+
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.json({msg: "Welcome to the app"});
+})
+
+app.listen(process.env.PORT, () => {
+    console.log(`Listening on port ${process.env.PORT}`);
+})

2f1a7cfa9dc3fb68adb88706f894b5ed4c7328fb
Cleggatron <david.robert.clegg@gmail.com>
2022-07-31 09:22:31 +0100

    added some logging middleware

--- server/server.js
+++ server/server.js
@@ -4,6 +4,12 @@ require("dotenv").config();
 
 const app = express();
 
+//middleware
+app.use((req, res, next) => {
+    console.log(req.path, req.method)
+    next();
+})
+
 app.get("/", (req, res) => {
     res.json({msg: "Welcome to the app"});
 })

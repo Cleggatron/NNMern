3ba4bd567d175fe562646b5c776b999e2ddd8995
Cleggatron <david.robert.clegg@gmail.com>
2022-08-07 18:36:03 +0100

    added basic context

---
+++ frontend/src/context/WorkoutContext.js
@@ -0,0 +1,11 @@
+import {createContext} from "react";
+
+export const WorkoutContext = createContext();
+
+const WorkoutContextProvider = () => {
+    return (
+        <WorkoutContext.Provider>
+            
+        </WorkoutContext.Provider>
+    )
+}

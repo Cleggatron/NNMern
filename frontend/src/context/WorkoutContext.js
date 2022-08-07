df96bf81a331343e6ac34280fb8f7eb86cab664c
Cleggatron <david.robert.clegg@gmail.com>
2022-08-07 18:46:53 +0100

    added basic actions

--- frontend/src/context/WorkoutContext.js
+++ frontend/src/context/WorkoutContext.js
@@ -1,11 +1,33 @@
-import {createContext} from "react";
+import {createContext, useReducer} from "react";
 
 export const WorkoutContext = createContext();
 
-const WorkoutContextProvider = () => {
+export const workoutReducer = (state, action) => {
+    switch (action.type){
+        case "SET_WORKOUTS":
+            return {
+                workouts: action.payload
+            }
+
+        case "CREATE_WORKOUT": 
+            return {
+                workouts: [action.payload, ...state.workouts]
+            }
+
+        default: 
+        return state
+
+    }
+}
+
+export const WorkoutContextProvider = ({children}) => {
+
+    const [state, dispatch] = useReducer(workoutReducer, {
+        workouts: null
+    });
     return (
-        <WorkoutContext.Provider>
-            
+        <WorkoutContext.Provider value={state}>
+            {children}
         </WorkoutContext.Provider>
     )
 }
